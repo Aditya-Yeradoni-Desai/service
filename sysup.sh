3e5415ce2e6e03da58bdaaa821e5c1e4a316124e
Aditya-Yeradoni-Desai <ydaditya2022@gmail.com>
2026-05-26 01:19:57 +0530

    final copy of sysup.sh
    bash script for running 3 commands at once to reduce manual work.

---
+++ sysup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "━━━━━━━━━━━ SYSTEM UPDATE ━━━━━━━━━━━"
+ping -c 1 google.com > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "[✘] No internet connection"
+    exit 1
+fi
+echo "Internet connection detected"
+echo
+echo "━━━━━━━━━━━ UPDATING PACKAGES ━━━━━━━━━━━"
+sudo apt update
+if [ $? -ne 0 ]; then
+    echo "[✘] Update failed"
+    exit 1
+fi
+echo
+echo "━━━━━━━━━━━ UPGRADING SYSTEM ━━━━━━━━━━━"
+sudo apt full-upgrade -y
+if [ $? -ne 0 ]; then
+    echo "[✘] Upgrade failed"
+    exit 1
+fi
+echo
+echo "━━━━━━━━━━━ REMOVING UNUSED PACKAGES ━━━━━━━━━━━"
+sudo apt autoremove -y
+if [ $? -ne 0 ]; then
+    echo "[✘] Autoremove failed"
+    exit 1
+fi
+clear
+echo "━━━━━━━━━━━ COMPLETE ━━━━━━━━━━━"
+echo "System successfully updated"
