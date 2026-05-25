c48ee69d51c149fb5cd0f28fac6583812cfcab09
Aditya-Yeradoni-Desai <ydaditya2022@gmail.com>
2026-05-26 01:26:39 +0530

    Update sysup.sh

--- sysup.sh
+++ sysup.sh
@@ -2,7 +2,7 @@
 echo "━━━━━━━━━━━ SYSTEM UPDATE ━━━━━━━━━━━"
 ping -c 1 google.com > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo "[✘] No internet connection"
+    echo "No internet connection"
     exit 1
 fi
 echo "Internet connection detected"
@@ -10,14 +10,14 @@ echo
 echo "━━━━━━━━━━━ UPDATING PACKAGES ━━━━━━━━━━━"
 sudo apt update
 if [ $? -ne 0 ]; then
-    echo "[✘] Update failed"
+    echo "Update failed"
     exit 1
 fi
 echo
 echo "━━━━━━━━━━━ UPGRADING SYSTEM ━━━━━━━━━━━"
 sudo apt full-upgrade -y
 if [ $? -ne 0 ]; then
-    echo "[✘] Upgrade failed"
+    echo "Upgrade failed"
     exit 1
 fi
 echo
